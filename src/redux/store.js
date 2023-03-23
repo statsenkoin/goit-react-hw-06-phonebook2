3b7f1a6d20780cef509177db8cd67b0835a7d8bb
Ihor Statsenko <statsenkoin@gmail.com>
2023-03-23 23:50:05 +0200

    Feature/ add defaultMiddleware

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,8 +3,17 @@ import { contactsReducer } from './contactsSlice';
 import { filterReducer } from './filterSlice';
 
 import storage from 'redux-persist/lib/storage';
-import { persistReducer, persistStore } from 'redux-persist';
-import thunk from 'redux-thunk';
+import {
+  persistReducer,
+  persistStore,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+// import thunk from 'redux-thunk';
 
 const persistConfig = {
   key: 'users',
@@ -21,7 +30,13 @@ const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
   reducer: persistedReducer,
-  middleware: [thunk],
+  // middleware: [thunk],
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
 
 export const persistor = persistStore(store);
