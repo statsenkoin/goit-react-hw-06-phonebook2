a1aa85da8ee67acd2bebac09b891365de1dea906
Ihor Statsenko <statsenkoin@gmail.com>
2023-03-23 23:56:44 +0200

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,6 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import { contactsReducer } from './contactsSlice';
 import { filterReducer } from './filterSlice';
-
 import storage from 'redux-persist/lib/storage';
 import {
   persistReducer,
